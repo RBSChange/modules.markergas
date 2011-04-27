ed837bc740a8e566049b4cdb9bdf13246338108c
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-04-27 17:51:17 +0200

    [FIX #28471] Suivi e-commerce Google Analytics

--- lib/blocks/BlockEcommerceTrackingAction.class.php
+++ lib/blocks/BlockEcommerceTrackingAction.class.php
@@ -8,37 +8,53 @@ class markergas_BlockEcommercetrackingAction extends website_BlockAction
 	 * @param f_mvc_Response $response
 	 * @return String
 	 */
-	function execute($request, $response)
-	{
-		if ($this->isInBackoffice())
-		{
-			return website_BlockView::NONE;
-		}
-		$cartInfo = order_CartService::getInstance()->getDocumentInstanceFromSession();
-		if ($cartInfo !== null)
-		{
-			$order = $cartInfo->getOrder();
-			if ($order instanceof order_persistentdocument_order)
-			{	
-				$bills = order_BillService::getInstance()->getByOrder($order);
-				$bill = count($bills) == 0 ? null : f_util_ArrayUtils::firstElement($bills);
-				if ($this->getConfiguration()->getTrackonlypaidorders() ? ($bill && $bill->getStatus() !== order_BillService::FAILED) : true)
-				{
-					$website = website_WebsiteModuleService::getInstance()->getCurrentWebsite();
-					$lang = RequestContext::getInstance()->getLang();
-					$markers = website_MarkerService::getInstance()->getByWebsiteAndLang($website, $lang);
-					foreach ( $markers as $marker )
-					{
-						if ($marker instanceof markergas_persistentdocument_markergas && $marker->getUseEcommerce() && in_array($order->getBillingModeId(), DocumentHelper::getIdArrayFromDocumentArray($marker->getBillingmodesArray())))
-						{
-							$html = markergas_MarkergasService::getInstance()->getEcommercePlainMarker($order, $marker, $this->getConfiguration()->getIncludetaxes());
-							$this->getContext()->appendToPlainMarker($html);
-							break;
-						}
-					}
-				}
-			}
-		}
-		return website_BlockView::NONE;
-	}
+        function execute($request, $response)
+        {
+                if ($this->isInBackoffice())
+                {
+                        return website_BlockView::NONE;
+                }
+
+                $order = $this->getCurrentOrder();
+                if ($order instanceof order_persistentdocument_order)
+                {
+                        $bills = order_BillService::getInstance()->getByOrder($order);
+                        $bill = f_util_ArrayUtils::firstElement($bills);
+                        if ($this->getConfiguration()->getTrackonlypaidorders() ? ($bill && $bill->getStatus() !== order_BillService::FAILED) : true)
+                        {
+                                $website = website_WebsiteModuleService::getInstance()->getCurrentWebsite();
+                                $lang = RequestContext::getInstance()->getLang();
+                                $markers = website_MarkerService::getInstance()->getByWebsiteAndLang($website, $lang);
+                                foreach ( $markers as $marker )
+                                {
+                                        if ($marker instanceof markergas_persistentdocument_markergas && $marker->getUseEcommerce() && in_array($order->getBillingModeId(), DocumentHelper::getIdArrayFromDocumentArray($marker->getBillingmodesArray())))
+                                        {
+                                                $html = markergas_MarkergasService::getInstance()->getEcommercePlainMarker($order, $marker, $this->getConfiguration()->getIncludetaxes());
+                                                $this->getContext()->appendToPlainMarker($html);
+                                                break;
+                                        }
+                                }
+                        }
+                }
+                return website_BlockView::NONE;
+        }
+
+        private function getCurrentOrder()
+        {
+                $orderParams = $this->getHTTPRequest()->getModuleParameters("order");
+                if (isset($orderParams["orderId"]))
+                {
+                        $orderId = $orderParams["orderId"];
+                        if (intval($orderId))
+                        {
+                                $order = DocumentHelper::getDocumentInstance($orderId, 'modules_order/order');
+                                $customer = customer_CustomerService::getInstance()->getCurrentCustomer();
+                                if (DocumentHelper::equals($customer, $order->getCustomer()))
+                                {
+                                        return $order;
+                                }
+                        }
+                }
+                return null;
+        }
 }
