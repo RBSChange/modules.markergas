40f6493244535f07151abb93e91312e7cdd03643
Gaël PORT <gael.port@rbs.fr>
2011-07-29 16:01:27 +0200

    [REFACTOR] Move StyleService and JsService and related classes to website module.

--- lib/blocks/BlockDashboardGoogleAnalyticsAction.class.php
+++ lib/blocks/BlockDashboardGoogleAnalyticsAction.class.php
@@ -2,19 +2,17 @@
 class markergas_BlockDashboardGoogleAnalyticsAction extends website_BlockAction
 {	
 	/**
-	 * @see website_BlockAction::execute()
-	 *
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
 	 * @return String
 	 */
-	function execute($request, $response)
+	public function execute($request, $response)
 	{
 		if ($this->isInBackofficeEdition())
 		{
 			return website_BlockView::DUMMY;
 		}
-		StyleService::getInstance()->registerStyle('modules.markergas.dashboardGoogleAnalytics');
+		website_StyleService::getInstance()->registerStyle('modules.markergas.dashboardGoogleAnalytics');
 		return website_BlockView::SUCCESS;
 	}
 }
